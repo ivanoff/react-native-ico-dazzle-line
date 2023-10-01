98aeebca8a7d8dbf4ae769eb251ed7768fca4c53
Dimitry Ivanov <2@ivanoff.org.ua>
2023-10-01 18:38:01 +0300

    fix types

--- typings.d.ts
+++ typings.d.ts
@@ -1,5 +1,6 @@
 declare module 'react-native-ico-dazzle-line' {
     import { ReactNode } from 'react';
+    import { SvgProps, AdditionalProps } from 'react-native-svg';
 
     type iconNames = 'address-book' |
       'address-card' |
@@ -1749,7 +1750,7 @@ declare module 'react-native-ico-dazzle-line' {
       badge?: number | string | badgeTypeObj;
     }
 
-    const Icon: (props: IconProps) => ReactNode;
+    const Icon: (props: IconProps & SvgProps & AdditionalProps) => ReactNode;
 
     export { iconNames };
     export default Icon;
